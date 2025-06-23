87fbde53592496e60b335f419a58bf0b48aaeb03
aumtch1234 <taweehok.k@ku.th>
2025-06-23 15:27:30 +0700

    Second commit

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,18 +1,29 @@
-// routes/allRoutes.js หรือไฟล์ router ที่ใช้
-
+// routes/allRoutes.js
 const express = require('express');
 const router = express.Router();
+
 const { login, addAdmin } = require('../controllers/authController');
-const {getAllAdmins, verifyAdmin, getPendingAdmins,} = require('../controllers/allAdminController');
+const { getAllAdmins, verifyAdmin, getPendingAdmins } = require('../controllers/allAdminController');
+const { createFood, getFoods, deleteFood, updateFood } = require('../controllers/FoodController');
 const { verifyToken, requireVerified } = require('../middlewares/authMiddleware');
 
+const multer = require('multer');
+const { storage } = require('../utils/cloudinary');
+const upload = multer({ storage });
+
+// Auth
 router.post('/login', login);
 router.post('/add-admin', addAdmin);
 
-router.patch('/admins/verify/:id', verifyToken, requireVerified, verifyAdmin);  // เพิ่ม verifyAdmin
-
-router.get('/admins/pending', getPendingAdmins); // <-- บรรทัดที่ 15
-
+// Admin Verification
+router.patch('/admins/verify/:id', verifyToken, requireVerified, verifyAdmin);
+router.get('/admins/pending', getPendingAdmins);
 router.get('/admins/all', verifyToken, requireVerified, getAllAdmins);
+
+// Food Menu
+router.post('/addfood', verifyToken, requireVerified, upload.single('image'), createFood);
+router.get('/foods' , getFoods);
+router.delete('/foods/:id', verifyToken, requireVerified, deleteFood);
+router.put('/foods/:id', verifyToken, requireVerified, upload.single('image'), updateFood);
 
 module.exports = router;
